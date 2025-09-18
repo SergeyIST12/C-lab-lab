d395083ee252d6bd96897f0b012822db9b936754
SergeyIST12 <3108200631@mail.ru>
2025-09-18 23:16:17 +0400

    Add files via upload

---
+++ С шарп/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Player1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtOperand2 = new System.Windows.Forms.TextBox();
+            this.txtOperation = new System.Windows.Forms.TextBox();
+            this.txtOperand1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.txtOperand2);
+            this.panel1.Controls.Add(this.txtOperation);
+            this.panel1.Controls.Add(this.txtOperand1);
+            this.panel1.Location = new System.Drawing.Point(77, 121);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(211, 217);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtOperand2
+            // 
+            this.txtOperand2.Location = new System.Drawing.Point(38, 168);
+            this.txtOperand2.Name = "txtOperand2";
+            this.txtOperand2.Size = new System.Drawing.Size(126, 20);
+            this.txtOperand2.TabIndex = 2;
+            // 
+            // txtOperation
+            // 
+            this.txtOperation.Location = new System.Drawing.Point(38, 110);
+            this.txtOperation.Name = "txtOperation";
+            this.txtOperation.Size = new System.Drawing.Size(126, 20);
+            this.txtOperation.TabIndex = 1;
+            this.txtOperation.TextChanged += new System.EventHandler(this.txtOperation_TextChanged);
+            // 
+            // txtOperand1
+            // 
+            this.txtOperand1.Location = new System.Drawing.Point(38, 56);
+            this.txtOperand1.Name = "txtOperand1";
+            this.txtOperand1.Size = new System.Drawing.Size(126, 20);
+            this.txtOperand1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Silver;
+            this.panel2.Controls.Add(this.lblResult);
+            this.panel2.Controls.Add(this.btnCalculate);
+            this.panel2.Location = new System.Drawing.Point(471, 121);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(233, 217);
+            this.panel2.TabIndex = 1;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(30, 147);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 1;
+            this.lblResult.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.Color.Red;
+            this.btnCalculate.Location = new System.Drawing.Point(33, 56);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(162, 53);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Вычислить";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtOperand2;
+        private System.Windows.Forms.TextBox txtOperation;
+        private System.Windows.Forms.TextBox txtOperand1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
